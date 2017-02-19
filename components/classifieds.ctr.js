98801cda1d48a69c18264630bdd203813fb4d5f8
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-02-19 13:48:54 -0800

    set up app by adding ctrl, app.js, css and updating the index.html

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,12 @@
+(function() {
+
+  "use strict";
+
+ angular.module('ngClassifieds').controller("classifiedsCtrl", function($scope) {
+   $scope.name = {
+     first: "Josh",
+     last: "Collins"
+   };
+   $scope.message = "Hello, world!!!"
+ });
+})();
