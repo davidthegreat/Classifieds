84b7b9ca0e64a04b061b17d963cb3204a6f3b098
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-02-20 01:16:32 -0800

    add two functions one to close and another to open a navbar

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -2,12 +2,20 @@
 
   "use strict";
 
- angular.module('ngClassifieds').controller("classifiedsCtrl", function($scope, $http, classfiedsFactory) {
+ angular.module('ngClassifieds').controller("classifiedsCtrl", function($scope, $http, classfiedsFactory, $mdSidenav) {
   classfiedsFactory.getClassifieds().then(function(classifieds){
 
      $scope.classifieds = classifieds.data
     // console.log(data)
    })
+
+   $scope.openSidebar = function() {
+     $mdSidenav('left').open();
+   }
+
+   $scope.closeSidebar = function() {
+     $mdSidenav('left').close();
+   }
 
  });
 })();
